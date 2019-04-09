8f23931c286284129b22f0c96bbaa720104c8eb2
Christian Nunciato <c@nunciato.org>
2019-04-08 21:56:25 -0700

    Pull parse and process into subdirs
    Signed-off-by: Christian Nunciato <c@nunciato.org>

--- Makefile
+++ Makefile
@@ -1,42 +1,53 @@
+.PHONY: build
 build:
 	rm -rf site/public
 	node_modules/.bin/node-sass --include-path site/scss --output site/static/css site/scss/main.scss
 	hugo --baseURL https://chris.nunciato.org --source site
 
+.PHONY: deploy
 deploy: build
 	$(MAKE) update
 	$(MAKE) invalidate
 
+.PHONY: preview
 preview:
 	pulumi preview
 
+.PHONY: update
 update:
 	pulumi up --yes --skip-preview
 
+.PHONY: invalidate
 invalidate:
 	aws cloudfront create-invalidation \
 		--distribution-id $(shell pulumi stack output cloudfrontDistributionId) \
 		--paths $(shell find site/public -name "*.html" -o -name "*.css" -o -name "*.js" | sed "s/^site\/public//g")
 
+.PHONY: process
 process:
 	docker run -it -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v ~/Desktop/Exports:/media cnunciato/process
 
-process-local:
-	./scripts/process.sh ~/Desktop/Exports
+.PHONY: process_local
+process_local:
+	./process/process.sh ~/Desktop/Exports
 
+.PHONY: clean
 clean:
 	rm -rf node_modules
 	rm -rf package-lock.json
 
+.PHONY: ensure
 ensure:
 	npm install
 
+.PHONY: serve
 serve:
 	open "http://localhost:1313/"
 	pushd site && \
 	hugo serve && \
 	popd
 
+.PHONY: serve_public
 serve_public:
 	$(MAKE) build
 	cd site/public
@@ -44,11 +55,14 @@ serve_public:
 	php -S localhost:1314
 	cd ..
 
+.PHONY: watch_sass
 watch_sass:
 	node_modules/.bin/node-sass --include-path site/scss --output site/static/css site/scss/main.scss --watch
 
+.PHONY: work
 work:
 	code website.code-workspace
 
+.PHONY: travis
 travis:
 	./scripts/travis_push.sh
---
+++ parse/app/index.js
@@ -0,0 +1,135 @@
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const USER = 'cnunciato';
+const TOKEN = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
+const REPO = 'github.com/cnunciato/website'
+const UPLOADS = 'uploads';
+
+// Set up Git
+const remote = `https://${USER}:${TOKEN}@${REPO}`;
+
+// App
+const fs = require('fs');
+const yaml = require('yamljs');
+const execSync = require('child_process').execSync;
+const express = require('express');
+const multer  = require('multer');
+const upload = multer({ dest: UPLOADS });
+const rimraf = require("rimraf");
+const request = require("request");
+const app = express();
+
+rimraf.sync(UPLOADS);
+fs.mkdirSync(UPLOADS);
+
+app.get('/', (req, res) => {
+  res.send('Hi, world. 👋');
+});
+
+app.post('/', upload.any(), function (req, res, next) {
+
+    // TODO: Should probably validate this is from a particular address, or something.
+    // Just so it isn't wide upen to the world.
+
+    // TODO: Weird things happen when we export from Lightroom on the iPhone -- it appears
+    // to strip out all the useful EXIF data, and the dates get weird, too. So we should
+    // probably support some kind of a way to share a Lightroom share URL or something, but
+    // it's not pressing right now.
+
+    // Much cooler would be to support video clips. NEXT!
+
+    console.log(req.files);
+
+    req.files.forEach(file => {
+
+        // TODO: Handle more than just JPGs! No reason not to.
+        // And what about multiple attachments? That should/could work too, no?
+
+        if (file.mimetype === "image/jpeg") {
+            const path = file.path;
+            const name = file.filename;
+
+            // Make a folder to contain the new file, so we can process it.
+            fs.mkdirSync(name);
+
+            // Make a media folder under that.
+            fs.mkdirSync(`${name}/media`);
+
+            // Copy the file to the media folder, renaming it at the same time.
+            // TODO: You'll need to update this extension here.
+            fs.copyFileSync(path, `${name}/media/${name}.jpg`);
+
+            // Clone the website repo, which gives us access to the processing scripts.
+            execSync(`git clone ${remote} ${name}/website`, { stdio: "inherit" });
+
+            // Process the file, by processing all files in the media directory. Send the output to a file.
+            execSync(`${name}/website/process/process.sh ${name}/media | tee ${name}/out.yaml`, { stdio: "inherit" });
+
+            // TODO: All bets are off if the push to S3 failed. Exit loudly here and do not push to GitHub.
+            // Note that this may be something we need to do in the script itself.
+
+            // Read the output into a string.
+            const output = fs.readFileSync(`${name}/out.yaml`, 'utf-8');
+
+            if (!output || output === "") {
+                console.error("No output!");
+                return;
+            }
+
+            // Take the output from that process and turn it into a JSON object.
+            const result = yaml.parse(output.substr(output.indexOf("- type: photo")));
+            const photo = result[0];
+
+            // Make a new post, using the subject line of the email.
+            const filepath = `site/content/photos/${req.body.subject.toLowerCase().replace(/ /g, "-")}.md`;
+
+            // Update the frontmatter
+            const frontmatter = {
+                title: req.body.subject,
+                date: new Date(),
+                draft: false,
+                photo: {
+                    url: photo.url,
+                    thumb: photo.thumb,
+                    preview: photo.preview,
+                    created: photo.created,
+                    exif: photo.exif,
+                    title: photo.title,
+                    caption: photo.caption,
+                }
+            };
+
+            const contents = `---\n${yaml.stringify(frontmatter, 4)}\n---\n`;
+
+            // Post a new file to GitHub with that output.
+            request.put(`https://api.github.com/repos/cnunciato/website/contents/${filepath}`, {
+                headers: {
+                    "User-Agent": "Chris's Parser/Uploader"
+                },
+                auth: {
+                    username: "cnunciato",
+                    password: TOKEN,
+                },
+                json: {
+                    message: "Add a photo",
+                    committer: {
+                        name: "Christian Nunciato",
+                        email: "c@nunciato.org",
+                    },
+                    content: Buffer.from(contents).toString('base64'),
+                },
+            }, (err, res) => {
+                if (err) {
+                    console.err(err);
+                }
+                console.log(res);
+            });
+        }
+    });
+
+    res.sendStatus(200);
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
